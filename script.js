f79cc69e7f2f73256f6ebc497c5d7c238ab7fe53
Wei-Cheng Yang <rainyday309@gmail.com>
2016-03-30 11:29:15 +0800

    passing data between child components

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var Product = React.createClass({
 
   buy: function () {
     this.setState({ qty: this.state.qty + 1 });
+    this.props.handleTotal(this.props.price);
     alert("you bought a phone!");
   },
   show: function () {
@@ -52,7 +53,9 @@ var Total = React.createClass({
       React.createElement(
         "h3",
         null,
-        " Total Cash: "
+        " Total Cash: $",
+        this.props.total,
+        " "
       )
     );
   }
@@ -61,6 +64,14 @@ var Total = React.createClass({
 // pass data to component: use html data //
 // use data passed in component: {this.props.[data-name]} //
 var ProductList = React.createClass({
+  // add a state of total, initialized to 0//
+  getInitialState: function () {
+    return { total: 0 };
+  },
+  calculateTotal: function (price) {
+    this.setState({ total: this.state.total + price });
+  },
+
   showProduct: function (name) {
     alert("You selected " + name + "!");
   },
@@ -69,13 +80,16 @@ var ProductList = React.createClass({
     return React.createElement(
       "div",
       null,
-      React.createElement(Product, { name: "Iphone", price: "300",
-        handleShow: this.showProduct }),
-      React.createElement(Product, { name: "Zen", price: "200",
-        handleShow: this.showProduct }),
-      React.createElement(Product, { name: "Nokia", price: "20",
-        handleShow: this.showProduct }),
-      React.createElement(Total, null)
+      React.createElement(Product, { name: "Iphone", price: 300,
+        handleShow: this.showProduct,
+        handleTotal: this.calculateTotal }),
+      React.createElement(Product, { name: "Zen", price: 200,
+        handleShow: this.showProduct,
+        handleTotal: this.calculateTotal }),
+      React.createElement(Product, { name: "Nokia", price: 20,
+        handleShow: this.showProduct,
+        handleTotal: this.calculateTotal }),
+      React.createElement(Total, { total: this.state.total })
     );
   }
 });
--- script.jsx
+++ script.jsx
@@ -5,6 +5,7 @@ var Product = React.createClass({
 
   buy: function(){
    this.setState({qty: this.state.qty + 1});
+   this.props.handleTotal(this.props.price);
    alert("you bought a phone!");
   },      
   show: function(){
@@ -27,7 +28,7 @@ var Total = React.createClass({
   render: function() {
     return (
       <div>
-        <h3> Total Cash: </h3>
+        <h3> Total Cash: ${this.props.total} </h3>
       </div>
     );
   }
@@ -36,6 +37,14 @@ var Total = React.createClass({
 // pass data to component: use html data //
 // use data passed in component: {this.props.[data-name]} //
 var ProductList = React.createClass({
+  // add a state of total, initialized to 0//
+  getInitialState: function(){
+    return {total: 0};
+  },
+  calculateTotal: function(price){
+    this.setState({total: this.state.total + price});
+  },
+
   showProduct: function(name){
     alert("You selected " + name + "!");
   },
@@ -43,13 +52,16 @@ var ProductList = React.createClass({
   render: function() {
     return (
         <div>
-          <Product name="Iphone" price="300" 
-            handleShow={this.showProduct}/>
-          <Product name="Zen" price="200"
-            handleShow={this.showProduct}/>
-          <Product name="Nokia" price="20"
-            handleShow={this.showProduct}/>
-          <Total/>
+          <Product name="Iphone" price={300} 
+            handleShow={this.showProduct}
+            handleTotal={this.calculateTotal}/>
+          <Product name="Zen" price={200}
+            handleShow={this.showProduct}
+            handleTotal={this.calculateTotal}/>
+          <Product name="Nokia" price={20}
+            handleShow={this.showProduct}
+            handleTotal={this.calculateTotal}/>
+          <Total total={this.state.total}/>
         </div>
       );
   }
